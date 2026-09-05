3b15f5e6060828edb04233e2a18c7d52363ddfe9
04tmoody <travismoody04@gmail.com>
2026-09-05 11:54:52 -0600

    Implement setBoardToFen method

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Locale;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -12,7 +14,7 @@ public class ChessBoard {
         
     }
 
-    private ChessPiece[][] board = new ChessPiece[8][8];
+    private final ChessPiece[][] board = new ChessPiece[8][8];
 
     /**
      * Adds a chess piece to the chessboard
@@ -32,7 +34,54 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[position.getRow()-1][position.getColumn()-1];
+    }
+
+    /**
+     * Sets the board according to a given FEN string
+     * For example, the starting position would be
+     * "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+     */
+    private void setBoardToFen(String fen) {
+        String piecesData = fen.split(" ")[0]; //Gets just the piece setup portion
+
+        // Start adding pieces from the top left
+        int row = 8;
+        int col;
+
+        String[] rowsData = piecesData.split("/");
+        for (String rowData : rowsData) {
+            col = 0; // Start each row from the left
+            row--;
+            for (int i=0; i<rowData.length(); i++) {
+                String pieceData = rowData.charAt(i)+"";
+                if ("12345678".contains(pieceData)) { // Empty Space
+                    col += Integer.parseInt(pieceData);
+                } else { // Add a piece
+                    ChessGame.TeamColor color; // color of the piece to add
+                    if (pieceData.toUpperCase().equals(pieceData)) { // piece is uppercase (white)
+                        color = ChessGame.TeamColor.WHITE;
+                    } else { // piece is lowercase (black)
+                        color = ChessGame.TeamColor.BLACK;
+                    }
+                    ChessPiece.PieceType type = switch(pieceData.toUpperCase()) {
+                        case "P" -> ChessPiece.PieceType.PAWN;
+                        case "R" -> ChessPiece.PieceType.ROOK;
+                        case "N" -> ChessPiece.PieceType.KNIGHT;
+                        case "B" -> ChessPiece.PieceType.BISHOP;
+                        case "Q" -> ChessPiece.PieceType.QUEEN;
+                        case "K" -> ChessPiece.PieceType.KING;
+                        default -> null;
+                    };
+                    if (type==null) { // Add empty space if unknown piece
+                        col++;
+                        continue;
+                    }
+                    ChessPiece piece = new ChessPiece(color,type);
+                    board[row][col] = piece;
+                }
+            }
+        }
     }
 
     /**
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -10,7 +10,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
+    private ChessGame.TeamColor color;
+    private ChessPiece.PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.color = pieceColor;
+        this.type = type;
     }
 
     /**
