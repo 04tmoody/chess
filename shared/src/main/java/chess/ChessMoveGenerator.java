6f0a6b95c940d025d3cba68fa140e6a48fa105b1
04tmoody <travismoody04@gmail.com>
2026-09-05 14:33:13 -0600

    Implement Pawn Moves

--- shared/src/main/java/chess/ChessMoveGenerator.java
+++ shared/src/main/java/chess/ChessMoveGenerator.java
@@ -12,10 +12,17 @@ public class ChessMoveGenerator {
     }
 
     /**
+     * Forwards to the addMoveFull method with default capturing logic and no promotion
+     */
+    private static void addMove(Collection<ChessMove> moves, ChessPiece piece, ChessBoard board, ChessPosition endPosition, ChessPosition startPosition) {
+        addMoveFull(moves,piece,board,endPosition,startPosition,true,false,false);
+    }
+
+    /**
      * Takes a row and column and checks if it is a valid move on the board
      * If it is valid, it adds it to the array of moves.
      */
-    private static void addMove(Collection<ChessMove> moves, ChessPiece piece, ChessBoard board, ChessPosition endPosition, ChessPosition startPosition) {
+    private static void addMoveFull(Collection<ChessMove> moves, ChessPiece piece, ChessBoard board, ChessPosition endPosition, ChessPosition startPosition, boolean canCapture, boolean mustCapture, boolean promote) {
         int row = endPosition.getRow();
         int col = endPosition.getColumn();
 
@@ -25,13 +32,25 @@ public class ChessMoveGenerator {
         }
         ChessPiece squarePiece = board.getPiece(endPosition);
         // check if there is a piece on the square.
-        if (squarePiece!=null) {
-            // Checks if move is to friendly piece
-            if (piece.getTeamColor().equals(squarePiece.getTeamColor())) {
+        if (squarePiece==null) {
+            if (mustCapture) {return;} // Requires capturing for pawns
+        } else {
+            // Checks if move is to friendly piece (or enemy piece for pawns)
+            if (!canCapture || piece.getTeamColor().equals(squarePiece.getTeamColor())) {
                 return;
             }
         }
-        moves.add(new ChessMove(startPosition, endPosition, null));
+        ChessPiece.PieceType[] promotionPieces = {ChessPiece.PieceType.QUEEN,
+                ChessPiece.PieceType.ROOK, ChessPiece.PieceType.KNIGHT,
+                ChessPiece.PieceType.BISHOP};
+        if (promote) {
+            for (ChessPiece.PieceType pieceType : promotionPieces) {
+                moves.add(new ChessMove(startPosition, endPosition, pieceType));
+            }
+        } else {
+            moves.add(new ChessMove(startPosition, endPosition, null));
+        }
+
     }
 
     /**
@@ -147,6 +166,32 @@ public class ChessMoveGenerator {
         return moves;
     }
 
+    // PAWN MOVES
+    private static Collection<ChessMove> pawnMoves(ChessPiece piece, ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        // Determine what direction the pawn is moving in
+        int dy;
+        if (piece.getTeamColor()==ChessGame.TeamColor.BLACK) {
+            dy = -1;
+        } else {
+            dy = 1;
+        }
+
+        // Determine if Pawn is promoting
+        boolean promotion = (row+dy==1 || row+dy==8);
+
+        // Move forward without capturing
+        addMoveFull(moves,piece,board,new ChessPosition(row+dy,col), position,false, false,promotion);
+
+        // Move diagonally with capturing
+        addMoveFull(moves,piece,board,new ChessPosition(row+dy,col-1), position,true, true,promotion);
+        addMoveFull(moves,piece,board,new ChessPosition(row+dy,col+1), position,true, true,promotion);
+        return moves;
+    }
+
     /**
      * Returns the moves for a given piece on a given board in a given position
      */
@@ -157,7 +202,7 @@ public class ChessMoveGenerator {
             case ChessPiece.PieceType.ROOK -> rookMoves(piece,board,position);
             case ChessPiece.PieceType.BISHOP -> bishopMoves(piece,board,position);
             case ChessPiece.PieceType.QUEEN -> queenMoves(piece,board,position);
-            default -> new ArrayList<>();
+            case ChessPiece.PieceType.PAWN -> pawnMoves(piece,board,position);
         };
     }
 }
